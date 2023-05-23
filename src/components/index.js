27019626ace64f85a90e93554185ae45db27f1d4
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-22 22:24:48 -0300

    feature: implement Barrell index

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Button } from "./button";
+export { default as Container } from "./container";
+export { default as Footer } from "./footer";
+export { default as Label } from "./label";
+export { default as Navbar } from "./navbar";
+export { default as Topbar } from "./topbar";
+export { default as WhatsApp } from "./whatsapp";
---
+++ src/components/navbar/index.js
@@ -0,0 +1,125 @@
+import Link from "next/link";
+import Topbar from "../topbar";
+import Container from "../container";
+
+const Navbar = () => {
+  return (
+    <>
+      <Topbar text="¡Envíos en el día a Villa Urquiza!" />
+      <nav className="bg-light sticky top-0 z-20">
+        <Container className="text-dark flex flex-wrap items-center justify-between p-4 lg:p-0 lg:py-4">
+          <div className="logo w-1/2 font-black text-2xl lg:w-1/5">
+            <Link href="/">SALUDABLE</Link>
+          </div>
+          <div className="toggler w-1/2 text-right lg:hidden">
+            <button className="flex items-center ml-auto mr-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 9h16.5m-16.5 6.75h16.5"
+                />
+              </svg>
+              <span className="ml-2 text-sm">Menú</span>
+            </button>
+          </div>
+          <div className="search w-full mt-3 lg:w-3/5 lg:mt-0">
+            <div className="formGroup relative">
+              <input
+                type="text"
+                placeholder="Buscá tu producto!"
+                className="bg-slate-100 w-full rounded-full p-4 pr-14"
+              />
+              <div className="icon absolute right-4 top-1/2 -translate-y-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="menu w-full hidden lg:block lg:w-1/5">
+            <ul className="flex lg:justify-end">
+              <li className="ml-4">
+                <Link href="/whatsapp">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li className="ml-4">
+                <Link href="/my-profile">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
+                </Link>
+              </li>
+              <li className="ml-4">
+                <Link href="/cart" className="flex items-start">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                    />
+                  </svg>
+                  <div className="number bg-accent rounded-full w-4 h-4 flex items-center justify-center text-[0.55rem] text-light font-bold">
+                    0
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </Container>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
