5eb77a13840b649d0375d099d75e7c4ef5445ebb
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-22 22:23:54 -0300

    feature: implement WhatsApp

---
+++ src/components/whatsapp/index.js
@@ -0,0 +1,11 @@
+import { Button } from "../index";
+
+const WhatsApp = () => {
+  return (
+    <div className="fixed right-10 bottom-10 z-10">
+      <Button type="accent" icon="whatsapp" onClick={true} />
+    </div>
+  );
+};
+
+export default WhatsApp;
