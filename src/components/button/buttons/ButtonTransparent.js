9cc26e1210a66390499401bf8ea325d5b9bdd784
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-24 00:09:07 -0300

    chore: improve Buttons component

--- src/components/button/buttons/ButtonTransparent.js
+++ src/components/button/buttons/ButtonTransparent.js
@@ -1,6 +1,8 @@
-const ButtonTransparent = ({ label, url, icon }) => {
+const ButtonTransparent = ({ label, url, icon, className }) => {
   return (
-    <button className="bg-transparent text-accent py-3 px-12 rounded-full flex items-center justify-center font-bold uppercase text-sm ">
+    <button
+      className={`${className} bg-transparent text-accent py-3 px-12 rounded-full flex items-center justify-center font-bold uppercase text-sm`}
+    >
       {icon && icon}
       {label && <span className={`${icon && "ml-2"}`}>{label}</span>}
     </button>
--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,11 +1,18 @@
 import { ICONS } from "./ButtonIcons";
 import { TYPES } from "./ButtonTypes";
 
-const Button = ({ type, label, url, icon }) => {
+const Button = ({ type, label, url, icon, className }) => {
   const ButtonType = TYPES[type];
   const IconComponent = ICONS[icon];
 
-  return <ButtonType label={label} url={url} icon={IconComponent} />;
+  return (
+    <ButtonType
+      label={label}
+      url={url}
+      icon={IconComponent}
+      className={className}
+    />
+  );
 };
 
 export default Button;
