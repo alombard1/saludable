76f8abe3168216910c07920d04c74e3edd6bb979
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-26 10:18:17 -0300

    update: WhatsApp design

--- src/components/whatsapp/index.js
+++ src/components/whatsapp/index.js
@@ -2,7 +2,7 @@ import { Button } from "../index";
 
 const WhatsApp = () => {
   return (
-    <div className="fixed right-10 bottom-10 z-10">
+    <div className="fixed right-4 bottom-4 z-10 md:right-10 md:bottom-10">
       <Button type="accent" icon="whatsapp" onClick={true} />
     </div>
   );
