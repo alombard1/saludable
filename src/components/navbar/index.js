3f0edf3e05546d4af1b561a55c28e92ac41e1a66
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-26 10:15:30 -0300

    chore: improve navbar design

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -11,34 +11,15 @@ const Navbar = () => {
     <>
       <Topbar text="¡Envíos en el día a Villa Urquiza!" />
       <nav className="bg-light sticky top-0 z-20">
-        <Container className="text-dark flex flex-wrap items-center justify-between p-4 lg:p-0 lg:py-4">
-          <div className="logo w-1/2 font-black text-2xl lg:w-1/5">
-            <Link href="/">SALUDABLE</Link>
+        <Container className="text-dark flex flex-wrap items-center justify-between p-4 md:p-0 md:py-4">
+          <div className="logo w-1/2 font-black text-2xl hidden md:w-1/5 md:block">
+            <Link href="/">SALUD</Link>
           </div>
-          <div className="toggler w-1/2 text-right lg:hidden">
-            <button className="flex items-center ml-auto mr-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 9h16.5m-16.5 6.75h16.5"
-                />
-              </svg>
-              <span className="ml-2 text-sm">Menú</span>
-            </button>
-          </div>
-          <div className="search w-full mt-3 lg:w-3/5 lg:mt-0">
+          <div className="search w-3/5">
             <div className="formGroup relative">
               <input
                 type="text"
-                placeholder="Buscá tu producto!"
+                placeholder="Buscar productos"
                 className="bg-slate-100 w-full rounded-full p-4 pr-14"
               />
               <div className="icon absolute right-4 top-1/2 -translate-y-1/2">
@@ -59,9 +40,9 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <div className="menu w-full hidden lg:block lg:w-1/5">
+          <div className="menu w-2/5 md:w-1/5">
             <ul className="flex lg:justify-end">
-              <li className="ml-4">
+              <li className="ml-4 hidden lg:block">
                 <Link href="/whatsapp">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -79,7 +60,7 @@ const Navbar = () => {
                   </svg>
                 </Link>
               </li>
-              <li className="ml-4">
+              <li className="ml-4 hidden lg:block">
                 <Link href="/my-profile">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -117,6 +98,27 @@ const Navbar = () => {
                     {getCartLength()}
                   </div>
                 </Link>
+              </li>
+              <li className="ml-4 lg:hidden">
+                <div className="toggler text-right">
+                  <button className="flex items-center ml-auto mr-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.75 9h16.5m-16.5 6.75h16.5"
+                      />
+                    </svg>
+                    <span className="ml-2 text-sm">Menú</span>
+                  </button>
+                </div>
               </li>
             </ul>
           </div>
