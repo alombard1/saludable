ec57826d8574c9100d18a4285e71d119e620dc89
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-26 10:18:34 -0300

    update: Button

--- src/components/button/ButtonIcons.js
+++ src/components/button/ButtonIcons.js
@@ -1,9 +1,13 @@
 import IconCart from "./icons/cart";
+import IconMinus from "./icons/minus";
+import IconPlus from "./icons/plus";
 import IconWhatsApp from "./icons/whatsapp";
 import IconWheat from "./icons/wheat";
 
 export const ICONS = {
   cart: IconCart(),
+  minus: IconMinus(),
+  plus: IconPlus(),
   whatsapp: IconWhatsApp(),
   wheat: IconWheat(),
 };
