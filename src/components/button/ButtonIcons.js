cd392ebc4ee1c4b143a045cb6fd2c1d12c96a92f
alombard1 <alejandro.lombardi0@gmail.com>
2023-05-26 13:14:48 -0300

    feature: add Icons

--- src/components/button/ButtonIcons.js
+++ src/components/button/ButtonIcons.js
@@ -1,13 +1,23 @@
 import IconCart from "./icons/cart";
+import IconHelp from "./icons/help";
+import IconHome from "./icons/home";
+import IconInstagram from "./icons/instagram";
 import IconMinus from "./icons/minus";
 import IconPlus from "./icons/plus";
+import IconStar from "./icons/star";
+import IconUser from "./icons/user";
 import IconWhatsApp from "./icons/whatsapp";
 import IconWheat from "./icons/wheat";
 
 export const ICONS = {
   cart: IconCart(),
+  help: IconHelp(),
+  home: IconHome(),
+  instagram: IconInstagram(),
   minus: IconMinus(),
   plus: IconPlus(),
+  star: IconStar(),
+  user: IconUser(),
   whatsapp: IconWhatsApp(),
   wheat: IconWheat(),
 };
